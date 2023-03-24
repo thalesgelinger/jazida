29db4f6fa764b679fab053f3feb0216db3d6f94b
Thales Augusto Gelinger <thalesgelinger@Thaless-MacBook-Air.local>
2023-03-24 11:20:42 -0300

    feat: add native base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Jazida</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NativeBaseProvider>
+            <Box flex={1} justifyContent="center" alignItems="center" >Hello world</Box>
+        </NativeBaseProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
